148b5c162b12c9704373b76b25123a0723f94053
Jorge Padilla <jorgep4dill4@gmail.com>
2020-07-26 07:03:26 -0500

    withdraw_superplus bug

--- app/controllers/casino_controller.rb
+++ app/controllers/casino_controller.rb
@@ -1,6 +1,6 @@
 class CasinoController < ApplicationController
   def withdraw_superplus
-    players_coins = Player.reduce(0) {|sum,p| sum+get_balance(p)}
+    players_coins = Player.all.reduce(0) {|sum,p| sum+get_balance(p)}
     puts players_coins-get_server_balance
   end
   private
