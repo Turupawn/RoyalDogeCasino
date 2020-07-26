39d70abf9e2e554a40e8f8620ba07005cd4638c5
Jorge Padilla <jorgep4dill4@gmail.com>
2020-07-26 03:48:49 -0500

    withdraw_superplus

---
+++ app/controllers/casino_controller.rb
@@ -0,0 +1,30 @@
+class CasinoController < ApplicationController
+  def withdraw_superplus
+    players_coins = Player.reduce(0) {|sum,p| sum+get_balance(p)}
+    puts players_coins-get_server_balance
+  end
+  private
+    def get_balance player
+      client = DogecoinClient.new
+      balance = 0
+      if client.valid?
+        balance += client.get_received_by_address(player.deposit_address)
+      else
+       # TODO: Handle invalid client
+      end
+        player.withdraws.each do |withdraw|
+         balance-=withdraw.amount
+        end
+        player.chests.each do |chest|
+         balance-=chest.cost
+         balance+=chest.reward
+        end
+       return balance
+    end
+    def get_server_balance
+      client = DogecoinClient.new
+      balance = 0
+      return balance = client.get_balance
+    end
+  end
+end
---
+++ test/controllers/casino_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CasinoControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
