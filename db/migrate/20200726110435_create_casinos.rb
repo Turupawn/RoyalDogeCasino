9eddf9d5c15c00c817f09bb54699dc64c1650488
Jorge Padilla <jorgep4dill4@gmail.com>
2020-07-26 07:03:43 -0500

    refactor

--- db/migrate/20200726110435_create_casinos.rb
+++
@@ -1,8 +0,0 @@
-class CreateCasinos < ActiveRecord::Migration[6.0]
-  def change
-    create_table :casinos do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/models/casino_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CasinoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
