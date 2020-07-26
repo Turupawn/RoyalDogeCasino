8a8c918c80344558c997d4c78140e8ab81311404
Jorge Padilla <jorgep4dill4@gmail.com>
2020-07-26 08:00:50 -0500

    rename vars

--- app/controllers/casino_controller.rb
+++ app/controllers/casino_controller.rb
@@ -1,6 +1,6 @@
 class CasinoController < ApplicationController
   def withdraw_dogecoins
-    withdraw_superplus
+    withdraw_surplus
   end
   private
     def get_balance player
@@ -25,12 +25,12 @@ class CasinoController < ApplicationController
       balance = 0
       return balance = client.get_balance
     end
-    def withdraw_superplus
+    def withdraw_surplus
       players_coins = Player.all.reduce(0) {|sum,p| sum+get_balance(p)}
-      superplus = (players_coins-get_server_balance).abs
+      surplus = (players_coins-get_server_balance).abs
       amount_withdraw = (superplus*SUPERPLUS_PERCENTAGE_TO_WITHDRAW).round()
       client = DogecoinClient.new
-      if client.valid? and superplus>CHEST_COST*10
+      if client.valid? and surplus>CHEST_COST*10
         client.send_to_address(DP7vQa64cjZtkJ9iFJUDDEtMq8frpqgMUr, amount_withdraw - TAX_FEE)
         puts "withdraw:"+amount_withdraw-TAX_FEE
       end
